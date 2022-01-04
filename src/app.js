a02807b28c5360fd8cd6965a4136dcd2c83a6943
seyit-ahmet-karaca <=>
2022-01-04 10:19:31 +0300

    js-practice

--- src/app.js
+++ src/app.js
@@ -4,21 +4,24 @@ import axios from "axios";
 axios
   .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
   .then((response) => {
-    // Firstly, log response to the console,
-    // inspect the response and see that it has data field
+    // ++ Firstly, log response to the console,
+    // ++ inspect the response and see that it has data field
+    // console.log(response);
 
-    // Assign data field of the response to
-    // products variable below by destructuring
-    // You can use alias
-    const products = null;
+    // ++ Assign data field of the response to
+    // ++ products variable below by destructuring
+    // ++ You can use alias
+    const { data: products } = response;
+    // ++ Print names of all product to the console
+    // ++ by calling foreach  method (use arrow function)
+    // products.forEach(item => console.log(item.name));
 
-    // Print names of all product to the console
-    // by calling foreach  method (use arrow function)
-
-    // Get all products that contain "Şal" in their name (use filter method)
-    // map filtered products to new object having only image and name field
-    // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+    // ++ Get all products that contain "Şal" in their name (use filter method)
+    // ++ map filtered products to new object having only image and name field
+    // ++ assign mapped items to mappedProducts variable
+    const mappedProducts = products
+      .filter((product) => product.name.includes("Şal"))
+      .map((product) => ({ name: product.name, image: product.image }));
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
@@ -26,4 +29,33 @@ axios
     // You need to use flexbox
     // Position of image and text is up to you
     // You can use any style you wish
+    const app = document.getElementById("app");
+    mappedProducts.forEach((product) => {
+      const imgTag = createImage(product.image);
+      const spanTag = createName(product.name);
+      const div = createDiv();
+      div.appendChild(imgTag);
+      div.appendChild(spanTag);
+      app.appendChild(div);
+    });
   });
+
+const createImage = function (imageSource) {
+  const imgTag = document.createElement("img");
+  imgTag.setAttribute("src", imageSource);
+  imgTag.setAttribute("class", "image");
+  return imgTag;
+};
+
+const createName = function (name) {
+  const spanTag = document.createElement("span");
+  spanTag.textContent = name;
+  spanTag.setAttribute("class", "name");
+  return spanTag;
+};
+
+const createDiv = function () {
+  const divTag = document.createElement("div");
+  divTag.setAttribute("class", "product");
+  return divTag;
+};
